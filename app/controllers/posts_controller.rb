f462207a0767ebec3b588008af48845d631c3907
Micah Banschick <micha.ban@gmail.com>
2020-12-07 20:01:13 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-
-    redirect_to post_path(@post)
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else 
+      render :new
+    end  
   end
 
   def edit
