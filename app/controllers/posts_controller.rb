2174d3d3cce6bc2dc02b5f9ce3a0e6775e3150c7
Micah Banschick <micha.ban@gmail.com>
2020-12-07 19:54:19 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.create(author_params)
+
+    redirect_to author_path(@author)
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
