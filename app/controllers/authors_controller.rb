cfc05c80ce823fd0a785323155bd6f1fc58c384b
Micah Banschick <micha.ban@gmail.com>
2020-12-07 19:55:22 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -13,6 +13,10 @@ class AuthorsController < ApplicationController
     redirect_to author_path(@author)
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def update
     @author = Author.find(params[:id])
 
